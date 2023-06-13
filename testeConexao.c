0e659ec9f3a28593e99e10c1d703a5572aefa9b8
duduantero <xduduanterox@gmail.com>
2023-06-13 06:30:46 -0300

    Add libmysql.lib and libmysql.dll

--- testeConexao.c
+++ testeConexao.c
@@ -8,7 +8,7 @@ int main() {
     MYSQL conexao;
     mysql_init(&conexao);
     // Aqui tenho um servidor mysql rodando no IP: 127.0.0.1 com usuário "root" sem senha "". ele conecta ao database "banco"
-    if ( mysql_real_connect(&conexao, "127.0.0.1", "root", "", "meubanco", 0, NULL, 0) )
+    if ( mysql_real_connect(&conexao, "127.0.0.1", "root", "", "test", 0, NULL, 0) )
     {
        printf("\nConexao ao banco realizada com sucesso!\n");
        mysql_query(&conexao,"INSERT INTO PESSOA(nome, sexo) values('Matheus Miranda', 'M');");
